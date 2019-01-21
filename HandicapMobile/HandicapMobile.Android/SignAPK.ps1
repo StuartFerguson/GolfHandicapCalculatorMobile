01eca124c24c45f0af723d47556f4b2b2db9ad30
StuartFerguson <stuart_ferguson1@btinternet.com>
2019-01-21 22:06:53 +0000

    Powershell Update

--- HandicapMobile/HandicapMobile.Android/SignAPK.ps1
+++ HandicapMobile/HandicapMobile.Android/SignAPK.ps1
@@ -1,7 +1,12 @@
-﻿& 'D:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\bin\MSBuild.exe' /t:Clean /p:Configuration=Release
+﻿$outputFolder=$args[0]
+
+& 'D:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\bin\MSBuild.exe' /t:Clean /p:Configuration=Release
 
 & 'D:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\bin\MSBuild.exe' /t:PackageForAndroid /p:Configuration=Release
 
 & 'c:\Program Files (x86)\Android\android-sdk\build-tools\26.0.2\zipalign.exe' -f -v 4 bin\Release\com.golfhandicapping.mobile.apk bin\Release\com.golfhandicapping.mobile-signed.apk
 
 & 'c:\Program Files (x86)\Android\android-sdk\build-tools\26.0.2\apksigner.bat' sign --ks golfmobile --ks-pass pass:Sc0tland bin\Release\com.golfhandicapping.mobile-signed.apk
+
+Copy-Item .\bin\Release\com.golfhandicapping.mobile-signed.apk -Destination D:\AndroidReleases\$outputFolder
+
